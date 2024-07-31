d3835fc72d4a482e9a6e1749f4f0470d808c2d29
kendii001 <kendijackline004@gmail.com>
2024-07-31 12:42:25 +0300

    add the files

---
+++ index.js
@@ -0,0 +1,14 @@
+const container=document.getElementById("container")
+const drop=document.getElementById("drop")
+// get element by id div 
+// get element by id dropdown
+// set on click div
+//
+container.onclick=()=>{ 
+if(container.clicked){
+   const drop=document.getElementById("drop").style.display="none";
+}
+  else{
+const drop= document.getElementById("drop").style.display="block";
+  } 
+} 
